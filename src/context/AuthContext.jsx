e48c9abfed036dd7e8219559573499374f340802
moldenburger <maritoldenburger@hotmail.com>
2025-03-31 15:50:52 +0200

    Finished main assignment

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,21 +1,31 @@
 import React, {createContext, useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 export const AuthContext = createContext({});
 
 export function AuthContextProvider({children}) {
-    const [isAuth, setIsAuth] = useState(false);
+    const [isAuth, toggleIsAuth] = useState(false);
+    const navigate = useNavigate();
 
-    const authData = {
-        isAuth,
-        login,
-        logout
-    };
+    function login() {
+        toggleIsAuth(true);
+        console.log("Gebruiker is ingelogd!");
+        navigate("/profile");
+    }
+
+    function logout() {
+        toggleIsAuth(false);
+        console.log("Gebruiker is uitgelogd!");
+        navigate("/");
+    }
+
+    const authData = {isAuth, login, logout};
 
     return (
-    <AuthContext.Provider value={authData}>
-        {children}
-    </AuthContext.Provider>
-)
+        <AuthContext.Provider value={authData}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
 
 export default AuthContextProvider;
